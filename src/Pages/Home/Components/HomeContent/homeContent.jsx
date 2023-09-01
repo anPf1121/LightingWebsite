d09ea52597872140705403f4e59992cc3804aef9
Pain281 <an.nguyenthien281@gmail.com>
2023-09-01 14:24:52 +0700

    update code

---
+++ src/Pages/Home/Components/HomeContent/homeContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+export default function HomeContent() {
+    return (
+        <>
+            <div className="top-content-title">
+                <h1>Main Title</h1>
+                <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam repellendus suscipit error exercitationem quam molestiae vel aspernatur amet voluptatem neque voluptatibus, tenetur eum quia deserunt temporibus. Mollitia, illo! Ea, quasi.</h2>
+            </div>
+        </>
+    )
+}
--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import HomeHeader from './Components/HomeHeader/homeHeader'
+import HomeContent from './Components/HomeContent/homeContent'
+import './home.css'
 // import 'react-sticky-header/styles.css';
 // import StickyHeader from 'ReactStickyHeader'
 export default function Home() {
   return (
     <>
       <HomeHeader />
+      <HomeContent />
       <div className="test"></div>
     </>
   )
